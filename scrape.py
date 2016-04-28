dfd8d070dfbfef34d4981da2cfb23c568d74e15c
Dan Robinson <danrobinson010@gmail.com>
2016-04-28 17:05:32 -0400

    added csv and ipython notebook

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 import requests
+import pandas as pd
 
 ROOT = 'https://hacker-news.firebaseio.com/v0/'
 
 top = requests.get(ROOT + 'topstories.json').json()
 
-articles = [['by', 'descendants', 'id', 'score', 'time', 'title', 'type', 'url']]
+articles = []
 
 i = 0
 for id in top:
@@ -15,8 +16,7 @@ for id in top:
         articles.append([a['by'], a['descendants'], a['id'], a['score'], a['time'], a['title'], a['type'], a['url']])
 
 
+df = pd.DataFrame.from_records(articles, columns=['by', 'descendants', 'id', 'score', 'time', 'title', 'type', 'url'])
 
-
-
-
+df.to_csv("articles.csv", encoding="utf-8", index=False)
 
