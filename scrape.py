329f726d54526768acc8243ac0f767f9606b346f
Reed Dunkle <reeddunkle@gmail.com>
2016-06-02 16:30:10 -0400

    Created new virtualenv, reinstalled packages to clean up the project. Created requirements.txt file.

--- scrape.py
+++ scrape.py
@@ -11,22 +11,34 @@ articles = []
 
 i = 0
 for id in top:
-    a = requests.get(ROOT + "item/" + str(id) + ".json").json()
-    print a
+    if i < 10:
 
-    if 'url' in a:
-        a_url = a['url']
-        g_article = g.extract(url=a_url)
-        a_page = requests.get(a_url)
-        #a_tree = html.fromstring(a_page.content)
-        a_html = a_page.content
-        a_title = g_article.title
-        a_text = g_article.cleaned_text
+        a = requests.get(ROOT + "item/" + str(id) + ".json").json()
+        print a
+        i += 1
 
-        articles.append([a['by'], a['descendants'], a['id'], a['score'], a['time'], a['title'], a['type'], a['url'], [a_html], [a_title], [a_text]])
+        if 'url' in a:
+            a_url = a['url']
+            g_article = g.extract(url=a_url)
+            a_page = requests.get(a_url)
+            #a_tree = html.fromstring(a_page.content)
+            a_html = a_page.content
+            a_title = g_article.title
+            a_text = g_article.cleaned_text
 
+            articles.append([a['by'], a['descendants'], a['id'], a['score'], a['time'], a['title'], a['type'], a['url'], [a_html], [a_title], [a_text]])
+    else:
+        pass
 
 df = pd.DataFrame.from_records(articles, columns=['by', 'descendants', 'id', 'score', 'time', 'title', 'type', 'url', 'html', 'title', 'text'])
 
 df.to_csv("articles.csv", encoding="utf-8", index=False)
+
+i = 0
+for article in articles:
+    i += 1
+    filename = "{0} - {1}.txt".format(i, article[2])
+    with open(filename, 'w') as outfile:
+        outfile.write(article[10])
+
 
